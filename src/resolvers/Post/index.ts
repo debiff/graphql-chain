0be87f53c971709ab85c38532aa661f33d7e5903
Simone Biffi <simone.biffi.91@gmail.com>
2023-08-04 11:29:08 +0200

    add post index

---
+++ src/resolvers/Post/index.ts
@@ -0,0 +1,30 @@
+import { ResolversDependencies } from "../index";
+import {
+  Post as GqlPost,
+  User as GqlUser,
+  Resolvers as GqlResolvers
+} from "../../generated/resolvers-types";
+import { Post } from "../../../data/Posts";
+import { UserResolver } from "./user";
+
+export const toGqlPost = ({
+  id,
+  title,
+  publication_date,
+  userId,
+  body
+}: Post): GqlPost => ({
+  __typename: "Post" as const,
+  id: id.toString(),
+  title,
+  published: publication_date !== null,
+  publication_date: publication_date !== null ? publication_date : undefined,
+  body,
+  userId,
+  user: null as unknown as GqlUser
+});
+export const PostResolver: (
+  deps: ResolversDependencies
+) => NonNullable<GqlResolvers["Post"]> = deps => ({
+  user: UserResolver(deps)
+});
